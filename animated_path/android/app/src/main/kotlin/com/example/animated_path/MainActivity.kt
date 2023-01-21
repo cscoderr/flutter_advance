7d0403b003d23f5a38d5f4dbfb3c5f1bea3c0ef2
Christopher Idowu <tommydprogrammer@gmail.com>
2023-01-21 02:27:41 +0100

    chore: initial commit

---
+++ animated_path/android/app/src/main/kotlin/com/example/animated_path/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animated_path
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ animated_path/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:animated_path/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
