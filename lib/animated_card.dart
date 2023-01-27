1da983fd7c974e896985806b1080598767ee7d5c
Christopher Idowu <tommydprogrammer@gmail.com>
2023-01-27 23:06:52 +0100

    chore: add animated card

---
+++ lib/animated_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnimatedCard extends StatelessWidget {
+  const AnimatedCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
